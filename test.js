7217b304b822ff6bbb105597df12608f443e88a6
rtritto <r.tritto5@studenti.uniba.it>
2022-06-07 19:29:29 +0200

    Add query options to getProxys

--- test.js
+++ test.js
@@ -1,12 +1,9 @@
 const P = require('./index')
 
-const t = new P();
+const t = new P()
 
 (async () => {
-  console.log(await t.getProxys(1))
-  console.log(await t.getProxys(2))
-  console.log(await t.getProxys(3))
+  console.log(await t.getProxys({ page: 1 }))
+  console.log(await t.getProxys({ page: 2 }))
+  console.log(await t.getProxys({ page: 3 }))
 })()
-
-
-
